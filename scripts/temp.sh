666f054316d4dd113c66635c3ed5c42370c3db21
Francesco <lescai@biomed.au.dk>
2020-05-28 18:17:24 +0100

    sync dev

--- scripts/temp.sh
+++ scripts/temp.sh
@@ -1,1 +1,5 @@
-perl -nae 'if($_=~/REGION/){print "#CHR\tSTART\tEND\tGFF\n"; next;} $indel="no"; if($F[3]=~/\+|-/){$indel="yes";} $ref=$F[3]; $ref=~s/\+|-//; $size=length($ref); $add=0; if($size >1){$add = $size - 1;} $start=$F[1]-1; $end=$F[1]+$add; print "$F[0]\t$start\t$end\t$F[14]\n";' SRR11494468_variants.tsv >SRR11494468_variants.bed
+variants=$1
+genome=$2
+name=${variants%.tsv}
+perl -nae 'if($_=~/REGION/){print "#CHR\tSTART\tEND\tGFF\n"; next;} $indel="no"; if($F[3]=~/\+|-/){$indel="yes";} $ref=$F[3]; $ref=~s/\+|-//; $size=length($ref); $add=0; if($size >1){$add = $size - 1;} $start=$F[1]-1; $end=$F[1]+$add; print "$F[0]\t$start\t$end\t$F[14]\n";' ${variants} >${name}.bed
+snpEff -i bed -ud 1 ${genome} ${name}.bed >${name}.anno
